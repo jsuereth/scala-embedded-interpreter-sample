fcccc9676c9537b9a5b7d6fe241499de0bf8b719
jsuereth <Joshua.Suereth@gmail.com>
2009-04-25 10:25:33 -0400

    Added Scripting Functionality, Auto-Imports, and 'Branding' to the InterpreterWrapper trait

--- src/main/scala/test/InterpreterWrapper.scala
+++ src/main/scala/test/InterpreterWrapper.scala
@@ -4,27 +4,51 @@ import scala.tools.nsc._
 import interpreter._
 import java.io._
 import scala.reflect._
+/**
+ * A trait to ease the embedding of the scala interpreter
+ */
 trait InterpreterWrapper {
 
   private var bindings : Map[String, (java.lang.Class[_], AnyRef)] = Map()
+  private var packageImports : List[String] = List()  
+  private var files = List[String]()
+  
   /**
    * Binds a given value into the interpreter when it starts with its most specific class
    */
-  def bind[A <: AnyRef](name : String, value : A)(implicit m : Manifest[A]) {
+  protected def bind[A <: AnyRef](name : String, value : A)(implicit m : Manifest[A]) {
     bindings +=  (( name,  (m.erasure, value)))
   }
   /**
    * Binds a given value itnot he interpreter with a given interface/higher-level class.
    */
-  def bindAs[A <: AnyRef, B <: A](name : String, interface : java.lang.Class[A], value : B) {
+  protected def bindAs[A <: AnyRef, B <: A](name : String, interface : java.lang.Class[A], value : B) {
     bindings += ((name,  (interface, value)))
   }
+  
+  /**
+   * adds an auto-import for the interpreter.
+   */
+  protected def autoImport(importString : String) {
+    packageImports = importString :: packageImports
+  }
+  
+  /**
+   * Adds a file that will be interpreter at the start of the interpreter
+   */
+  protected def addScriptFile(fileName : String) {
+    files = fileName :: files
+  }
+  
+  def helpMsg : String
+  def welcomeMsg : String
+  def prompt : String
   
   /**
    * This class actually runs the interpreter loop.
    */
   class MyInterpreterLoop(out : PrintWriter) extends InterpreterLoop(None, out) {
-    
+     override val prompt = InterpreterWrapper.this.prompt
     
      override def bindSettings() {       
        super.bindSettings()      
@@ -32,21 +56,32 @@ trait InterpreterWrapper {
          for( (name, (clazz, value)) <- bindings) {
            interpreter.bind(name, clazz.getCanonicalName, value)
          }
-         
-         
-        
+         for( importString <- packageImports) {
+        	 interpreter.interpret("import " + importString)
+         } 
        }
+     }
+    override def printHelp {
+       printWelcome()
+       out.println(helpMsg)
+       out.flush()
+     }
+     
+     override def printWelcome() {
+       out.println(welcomeMsg)
+       out.flush()
      }
   }
   
   def startInterpreting() {
-    val settings = new Settings()
+    val out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)))
+    val settings = new GenericRunnerSettings(out.println _)
+    files.foreach(settings.loadfiles.appendToValue)
     //Below for 2.8.0-SNAPSHOT
     //settings.completion.value = true
     
-    val interpreter = new MyInterpreterLoop(new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out))))
+    val interpreter = new MyInterpreterLoop(out)
     interpreter.main(settings)
-    
     ()
   }
 }
--- src/main/scala/test/Test.scala
+++ src/main/scala/test/Test.scala
@@ -1,10 +1,23 @@
 package test
 
+class MyClass(val name : String) {
+  def is = "Brillant!!!" //mispelled to be like PaulaBean
+  override def toString = "MyClass("+name+")"
+}
+
 object Test {
   
    def main(args : Array[String]) : Unit =  {
      val interpreter = new InterpreterWrapper() {
-       bind("josh", "awesome")
+       def prompt = "TestInterpreter> "
+       def welcomeMsg = """Welcome to Awesomeness!
+This is my version of the Scala interpreter"""
+       def helpMsg = """This is where I should
+list help of commands.  For now:
+:help - Prints this message
+:quit - Makes me sad"""
+       bind("josh", new MyClass("josh"))
+       autoImport("test._")
      }
      
      interpreter.startInterpreting
